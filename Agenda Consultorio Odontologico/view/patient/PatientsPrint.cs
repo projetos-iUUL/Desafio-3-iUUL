e724d518f52bc323fd238eba1b1eae2f072940d1
silveiraluisf <silveiraluis88@gmail.com>
2023-02-07 17:39:37 -0300

    refatorado impressão da lista de pacientes usando linq

--- Agenda Consultorio Odontologico/view/patient/PatientsPrint.cs
+++ Agenda Consultorio Odontologico/view/patient/PatientsPrint.cs
@@ -12,30 +12,19 @@ namespace Agenda_Consultorio_Odontologico.view.patientInterface
             Console.WriteLine("CPF          Nome                       Dt.Nasc.     Idade");
             Console.WriteLine("----------------------------------------------------------");
         }
-        public void ShowPatientsList(Patient patient)
-        {
-            using var context = new ConsultorioContext();
-            var appointments = context.Appointments.ToList();
-            string cpf = patient.CPF.ToString("00000000000");
-            string name = patient.Name;
-            string birth = patient.BirthDate.ToString("dd/MM/yyyy");
-            string age = patient.Age.ToString();
+        public void ShowPatientsList(string cpf, string name, string birth, string age)
+        {           
             Console.WriteLine(cpf.PadRight(13) + name.PadRight(27) + birth + age.PadLeft(6));
-            foreach (Appointment appointment in appointments)
-            {
-                if (appointment.Patient == patient && appointment.Date >= DateTime.Today)
-                {
-                    string date = appointment.Date.ToString("dd/MM/yyyy");
-                    string start = appointment.Start.ToString("0000");
-                    string end = appointment.End.ToString("0000");
-                    Console.WriteLine("             Agendado para: " + date);
-                    Console.WriteLine("             " + start + " às " + end);
-                }
-            }
         }
         public void Footer()
         {
             Console.WriteLine("----------------------------------------------------------");
+        }
+
+        internal void ShowPatientAppointment(string date, string start, string end)
+        {
+            Console.WriteLine("             Agendado para: " + date);
+            Console.WriteLine("             " + start + " às " + end);
         }
     }
 }
