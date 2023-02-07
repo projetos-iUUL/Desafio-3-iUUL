e5607dd9d45b6a6cb10253a503ec80de16f797e1
silveiraluisf <silveiraluis88@gmail.com>
2023-02-07 17:42:33 -0300

    refatorado impressão da lista de pacientes usando linq

--- Agenda Consultorio Odontologico/controller/patient/PatientMainController.cs
+++ Agenda Consultorio Odontologico/controller/patient/PatientMainController.cs
@@ -53,13 +53,26 @@ namespace Agenda_Consultorio_Odontologico.controller.patientControllers
             patientsPrint.Header();
 
             using var context = new ConsultorioContext();
-            var patients = context.Patients.ToList();
-
-            foreach (Patient patient in patients.OrderBy(x => x.Name))
+            var query = from pac in context.Patients
+                        join app in context.Appointments
+                        on pac.Id equals app.PatientId
+                        orderby pac.Name
+                        select new { pac, app };
+            foreach (var item in query)
             {
-                //patientsPrint.ShowPatientsList();
+                string cpf = item.pac.CPF.ToString("00000000000");
+                string name = item.pac.Name;
+                string birth = item.pac.BirthDate.ToString("dd/MM/yyyy");
+                string age = item.pac.Age.ToString();
+                patientsPrint.ShowPatientsList(cpf, name, birth, age);
+                if (item.pac.Appointments != null)
+                {
+                    string date = item.app.Date.ToString("dd/MM/yyyy");
+                    string start = item.app.Start.ToString("0000");
+                    string end = item.app.End.ToString("0000");
+                    patientsPrint.ShowPatientAppointment(date, start, end);
+                }
             }
-
             patientsPrint.Footer();
         }
 
